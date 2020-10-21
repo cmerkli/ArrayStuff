9a85428aca8af754be797df3acf3b4ec1f0fd660
cmerkli <71709518+cmerkli@users.noreply.github.com>
2020-10-21 14:09:36 -0400

    followed instructions

--- ArrayStuff.java
+++ ArrayStuff.java
@@ -13,31 +13,44 @@ public class ArrayStuff {
 
         // PART A
 
-        // TODO: Create integer array called "ages" with length 5.
+        // Create integer array called "ages" with length 5.
+        int [] ages = new int [5];
 
 
-        // TODO: Change the 3rd element of "ages" to be 18
+        // Change the 3rd element of "ages" to be 18
+        ages[3] = 18;
 
 
-        // TODO: Change the first element of "ages" to be an integer entered by the user
+        // Change the first element of "ages" to be an integer entered by the user
         System.err.println("Enter an integer:");
+        ages [0] = input.nextInt();
 
 
-        // TODO: Change the last element of "ages" to be the first element squared
+        // Change the last element of "ages" to be the first element squared
+        ages [4] = (int) Math.pow(ages[0], 2);
+ 
 
-
-        // TODO: Print all of the elements of "ages", one per line
+        // Print all of the elements of "ages", one per line
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+        System.out.println(ages[3]);
+        System.out.println(ages[4]);
+        
 
 
 
         // PART B
 
-        // TODO: Create AND initialize an array called "planets" that holds the
+        //Create AND initialize an array called "planets" that holds the
+        String [] planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
         // names of the planets in order from closest to the sun to farthest.
         // NOTE: Pluto is NOT a planet.
 
 
         // TODO: Print the first letter of the 7th planet from the sun
+       String planet7 = planets[6];
+       System.out.println(planet7.startsWith);
 
 
         // TODO: Change the name of our planet to be a line of text that the user inputs
